4478d8ac13b8098935dc6a892681948331eb9df6
Condrey <wangzdrey@gmail.com>
2025-04-29 12:49:51 +0300

    info feedback

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,14 @@
-import { Loader2Icon } from "lucide-react";
-import Image from "next/image";
+import { webName } from "@/lib/utils";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "404",
+  description: `The resource was not found in ${webName}`,
+};
 export default function Loading() {
   return (
     <div className="flex flex-col h-dvh gap-4 justify-center items-center">
-     <p>The resource you are looking for wasn't found</p>
+      <p>The resource you are looking for wasn't found</p>
     </div>
   );
 }

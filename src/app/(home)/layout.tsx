68456cfd34687b69fdc56c3ad0df614081b43741
Condrey <wangzdrey@gmail.com>
2025-10-04 09:51:47 +0300

    msg

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/user/footer";
 import TopAppBar from "@/components/user/top-app-bar";
 import { redirect } from "next/navigation";
 import SessionProvider from "../session-provider";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
 	const { session, user } = await validateRequest();
@@ -17,7 +18,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
 			<SidebarProvider>
 				<AppSidebar />
 				<SidebarInset>
-					<div className=" h-full overflow-hidden w-full">
+					{/* <div className=" h-full overflow-hidden w-full">
 						<header className="fixed top-0 z-50  w-full dark:bg-card bg-primary text-primary-foreground dark:text-card-foreground  dark:border-b">
 							<TopAppBar className="w-full max-w-9xl  py-2 mx-auto  px-3 " />
 						</header>
@@ -27,7 +28,8 @@ export default async function Layout({ children }: { children: React.ReactNode }
 								<Footer className="  bg-black/80 dark:bg-white/20 *:p-4 w-full  text-background dark:text-foreground " />
 							</footer>
 						</main>
-					</div>
+					</div> */}
+					<Card className="w-full   justify-center flex items-center	  min-h-dvh text-2xl  "><CardContent >Site under maintenance</CardContent></Card>
 				</SidebarInset>
 			</SidebarProvider>
 		</SessionProvider>

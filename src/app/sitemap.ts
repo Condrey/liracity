1c7051aa9fa56b2f81b380692669094e715c4661
Condrey <wangzdrey@gmail.com>
2025-11-07 18:38:53 +0300

    filtering

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import { getAllEvents } from "@/components/news-and-events/events/action";
 import { getAllNewsArticles } from "@/components/news-and-events/news/action";
+import { EventStatus, NewsArticleStatus } from "@/generated/prisma";
 import { MetadataRoute } from "next";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
@@ -8,20 +9,30 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const [allNewsArticles, allEvents] = await Promise.all([await getAllNewsArticles(), await getAllEvents()]);
 
 	// For news articles
-	const newsArticleSitemaps: MetadataRoute.Sitemap = allNewsArticles.map((n) => ({
-		url: `${baseUrl}/media/news-events/news/${n.slug}`,
-		lastModified: n.updatedAt,
-		changeFrequency: "always",
-		priority: 1
-	}));
+	const newsArticleSitemaps = allNewsArticles
+		.map((n) => {
+			if (n.status === NewsArticleStatus.PUBLISHED)
+				return {
+					url: `${baseUrl}/media/news-events/news/${n.slug}`,
+					lastModified: n.updatedAt,
+					changeFrequency: "always",
+					priority: 1
+				};
+		})
+		.filter(Boolean) as MetadataRoute.Sitemap;
 
 	// For events
-	const eventsSitemaps: MetadataRoute.Sitemap = allEvents.map((n) => ({
-		url: `${baseUrl}/media/news-events/events/${n.slug}`,
-		lastModified: n.updatedAt,
-		changeFrequency: "always",
-		priority: 1
-	}));
+	const eventsSitemaps = allEvents
+		.map((n) => {
+			if (n.status === EventStatus.PUBLISHED)
+				return {
+					url: `${baseUrl}/media/news-events/events/${n.slug}`,
+					lastModified: n.updatedAt,
+					changeFrequency: "always",
+					priority: 1
+				};
+		})
+		.filter(Boolean) as MetadataRoute.Sitemap;
 
 	return [
 		// Insert other pages

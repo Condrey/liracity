04623e22d1425515e9cf7b257fb85154d4413543
Condrey <wangzdrey@gmail.com>
2025-11-16 12:34:32 +0300

    transition

--- src/components/news-and-events/events/related-event-item.tsx
+++ src/components/news-and-events/events/related-event-item.tsx
@@ -28,24 +28,24 @@ export default function RelatedEventItem({ relatedEvent: item }: { relatedEvent:
 			size="sm"
 			onClick={() => startTransition(() => {})}
 			className={cn(
-				"px-2 py-1 bg-card dark:bg-muted hover:bg-muted hover:cursor-pointer",
+				"px-2 py-1 bg-card transition-shadow hover:shadow-md dark:bg-muted hover:bg-muted hover:cursor-pointer group/item",
 				isPending && "bg-muted animate-pulse"
 			)}
 			asChild
 		>
 			<Link href={getNavigationLinkWithPathnameWithoutUpdate(`/media/news-events/events/${item.slug}`)}>
 				{item.coverImage && (
-					<ItemMedia className="pointer-events-none touch-auto w-full flex *:flex-1">
+					<ItemMedia className="pointer-events-none overflow-hidden touch-auto w-full flex *:flex-1">
 						<ArticleImage
 							mediaIdentifier={item.coverImage.url}
 							height={180}
 							width={180}
-							className="h-[110px] w-full object-cover "
+							className="h-[110px] w-full object-cover transition-all group-hover/item:scale-110 "
 						/>
 					</ItemMedia>
 				)}
 				<ItemHeader className="flex-col items-start gap-0.5">
-					<ItemTitle className="border-b mb-1">{item.title}</ItemTitle>
+					<ItemTitle className="border-b mb-1 group-hover/item:font-bold transition-all">{item.title}</ItemTitle>
 					<div className="text-xs block text-start space-x-2 space-y-1.5">
 						{item.location && (
 							<address>
